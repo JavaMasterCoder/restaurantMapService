c3cfb95ba9d57e205b30845877044afd7530ebe0
Gregor <gregor_melikhov@mail.ru>
2020-03-24 15:19:51 +0300

    Added new test and methods for AdminUserDao

--- src/main/java/database/daos/AdminUserDAO.java
+++ src/main/java/database/daos/AdminUserDAO.java
@@ -3,6 +3,7 @@ package database.daos;
 import model.restaurants.Coordinate;
 import model.restaurants.Restaurant;
 import model.users.AbstractUser;
+import model.users.AdminUser;
 import model.users.EUserStatus;
 import model.users.Owner;
 import model.users.interfaces.IAdminUser;
@@ -18,10 +19,29 @@ public class AdminUserDAO implements IAdminUser {
         this.manager = manager;
     }
 
+    public AdminUser createAdminUser(String login, String password) {
+        AdminUser adminUser = new AdminUser(login, password);
+
+        manager.getTransaction().begin();
+
+        try {
+            manager.persist(adminUser);
+        } catch (Throwable cause) {
+            manager.getTransaction().rollback();
+            throw cause;
+        }
+
+        manager.getTransaction().commit();
+
+        return adminUser;
+    }
+
     @Override
     public void deleteUser(AbstractUser user) {
-            manager.createQuery("DELETE FROM AbstractUser user WHERE user.login = : login", AbstractUser.class)
-                    .setParameter("login", user.getLogin());
+            manager.createQuery("DELETE FROM AbstractUser user WHERE user.login = : login")
+                    .setParameter("login", user.getLogin())
+                    .executeUpdate();
+
     }
 
     @Override
@@ -29,7 +49,8 @@ public class AdminUserDAO implements IAdminUser {
         Owner owner = restaurant.getOwner();
 
         manager.createQuery("DELETE FROM Restaurant r WHERE r.coordinate = :coordinate", Restaurant.class)
-                .setParameter("coordinate", restaurant.getCoordinate());
+                .setParameter("coordinate", restaurant.getCoordinate())
+                .executeUpdate();
 
         owner.getRestaurants().remove(restaurant);
     }
@@ -38,7 +59,8 @@ public class AdminUserDAO implements IAdminUser {
     public void deleteComment(Restaurant restaurant, String comment) {
         manager.createQuery("UPDATE Restaurant r SET r.comments = :comment WHERE r.id = :restaurantId", Restaurant.class)
                 .setParameter("comment", restaurant.getComments())
-                .setParameter("restaurantId", restaurant.getId());
+                .setParameter("restaurantId", restaurant.getId())
+                .executeUpdate();
 
         restaurant.getComments().removeComment(comment);
     }
@@ -47,7 +69,8 @@ public class AdminUserDAO implements IAdminUser {
     public void changeUserStatus(AbstractUser user, EUserStatus newUserStatus) {
         manager.createQuery("UPDATE AbstractUser user SET user.userStatus = :newUserStatus WHERE user.id = :userId", AbstractUser.class)
                 .setParameter("newUserStatus", newUserStatus)
-                .setParameter("userId", user.getId());
+                .setParameter("userId", user.getId())
+                .executeUpdate();
 
         user.setUserStatus(newUserStatus);
     }
@@ -56,7 +79,8 @@ public class AdminUserDAO implements IAdminUser {
     public void changeRestaurantCoordinate(Restaurant restaurant, Coordinate newCoordinate) {
         manager.createQuery("UPDATE Restaurant r SET r.coordinate = :newCoordinate WHERE r.id = :restaurantId", Restaurant.class)
                 .setParameter("newCoordinate", newCoordinate)
-                .setParameter("restaurantId", restaurant.getId());
+                .setParameter("restaurantId", restaurant.getId())
+                .executeUpdate();
 
         restaurant.setCoordinate(newCoordinate);
     }
---
+++ src/test/java/daos/AdminUserDAOTest.java
@@ -0,0 +1,99 @@
+package daos;
+
+import database.daos.AdminUserDAO;
+import model.users.AbstractUser;
+import model.users.AdminUser;
+import model.users.EUserStatus;
+import model.users.User;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import static org.junit.Assert.*;
+
+public class AdminUserDAOTest {
+    private EntityManagerFactory factory;
+    private EntityManager manager;
+    private AdminUserDAO admins;
+
+    @Before
+    public void connect() {
+        factory = Persistence.createEntityManagerFactory("TestPersistanceUnit");
+        manager = factory.createEntityManager();
+        admins = new AdminUserDAO(manager);
+    }
+
+    @After
+    public void close() {
+        if (manager != null) {
+            manager.close();
+        }
+
+        if (factory != null) {
+            factory.close();
+        }
+    }
+
+    @Test
+    public void createAdminUserTest() {
+        String login = "adminTest";
+        String password = "pass";
+        AdminUser adminUser = admins.createAdminUser(login, password);
+
+        assertNotNull(adminUser);
+        assertEquals(login, adminUser.getLogin());
+        assertEquals(password, adminUser.getPassword());
+
+        AdminUser foundAdmin = manager.find(AdminUser.class, adminUser.getId());
+        assertNotNull(foundAdmin);
+        assertEquals(login, foundAdmin.getLogin());
+        assertEquals(password, foundAdmin.getPassword());
+
+        manager.refresh(foundAdmin);
+    }
+
+    @Test
+    public void changeUserStatusTest() {
+        String login = "adminTest1";
+        String password = "pass1";
+        AdminUser adminUser = admins.createAdminUser(login, password);
+
+        String userLogin = "user";
+        String userPassword = "password";
+        String name = "name1";
+
+        User user = new User(userLogin, userPassword, name);
+        manager.getTransaction().begin();
+        manager.persist(user);
+
+        AbstractUser foundUser = manager.find(AbstractUser.class, user.getId());
+        admins.changeUserStatus(foundUser, EUserStatus.REGISTERED);
+
+        assertEquals(EUserStatus.REGISTERED, foundUser.getUserStatus());
+
+        manager.refresh(foundUser);
+        assertEquals(EUserStatus.REGISTERED, foundUser.getUserStatus());
+    }
+
+    @Test
+    public void deleteUserTest() {
+        String userLogin = "user";
+        String userPassword = "password";
+        String name = "name1";
+
+        User user = new User(userLogin, userPassword, name);
+        manager.getTransaction().begin();
+        manager.persist(user);
+
+        AbstractUser foundUser = manager.find(AbstractUser.class, user.getId());
+        admins.deleteUser(foundUser);
+
+        foundUser = manager.find(AbstractUser.class, user.getId());
+        assertNull("User should be null", foundUser);
+    }
+}
